b380bb690a1d0a58a083f9fd668496bb948c1a4e
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-10-21 10:26:16 +0200

    Update Dashboard-app.py

--- Dashboard-app.py
+++ Dashboard-app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
+st.title(✈️ Dashboard Schiphol ✈️)
+
+st.markdown("""
+Welkom!\n
+\n
+Bij ons Dashboard over Schiphol als alles lukt in ieder geval....
+\n
+""")
