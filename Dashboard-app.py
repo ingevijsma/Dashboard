b1f09024806c53dce25bd0bb4e3d8471a7aa332e
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-11-02 15:20:40 +0100

    Update Dashboard-app.py

--- Dashboard-app.py
+++ Dashboard-app.py
@@ -5,11 +5,11 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.title("✈️ Dashboard Schiphol ✈️")
+st.title("✈️ Dashboard Eindpresentatie ✈️")
 
 st.markdown("""
 Welkom!\n
 \n
-Bij ons Dashboard over Schiphol als alles lukt in ieder geval....
+Bij ons Dashboard over onze eindpresenatie als alles lukt in ieder geval....
 \n
 """)
